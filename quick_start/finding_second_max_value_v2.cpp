387f1d48f3722963884da8d376246ccfb0f3dc2c
Katanych <k4t4ny4@mail.ru>
2021-08-04 12:58:09 +0700

    Add finging second max value v2

---
+++ quick_start/finding_second_max_value_v2.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+const int MAXN	  = 10000500;
+const int MAXVAL  = 1000000500;
+int n;
+int a[MAXN];
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+
+	int maxa = a[0];
+	int max2a = -MAXVAL;
+
+	for (int i = 1; i < n; ++i)
+		if (maxa <= a[i])
+		{
+			max2a = maxa;
+			maxa = a[i];
+		}
+
+	cout << max2a << "\n";
+	return 0;
+}
