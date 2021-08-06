cc6d4828f0942cc9027f44443f58a3dfcb7d78cb
Katanych <k4t4ny4@mail.ru>
2021-08-06 19:03:06 +0700

    add binary search algorithm

---
+++ quick_start/binary_search.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int MAXN = 200001;
+int a[MAXN];
+int n, m;
+
+string is_number_in(int x)
+{
+	int l = -1;
+	int r = n;
+
+	while (l + 1 < r)
+	{
+		int mid = (l + r) / 2;
+		if (a[mid] < x)
+			l = mid;
+		else
+			r = mid;
+	}
+
+	if (a[r] < x)
+		return "NO SOLUTION";
+	return to_string(a[r]);
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+
+	int x;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> x;
+		cout << is_number_in(x) << "\n";
+	}
+
+	return 0;
+}
