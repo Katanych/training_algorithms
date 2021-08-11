581650613259dce040a3cf349b607f36647d32a9
Katanych <k4t4ny4@mail.ru>
2021-08-11 11:41:49 +0700

    Add bin search by answer alg

---
+++ quick_start/binary_search_by_answer.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define ull unsigned long long
+
+ull dip_num(ull w, ull h, ull len)
+{
+	return (len / w) * (len / h);
+}
+
+ull find_ans(ull w, ull h, ull n)
+{
+	ull l = 0;
+	ull r = max(w, h) * n;
+
+	while (l + 1 < r)
+	{
+		ull mid = (l + r) / 2;
+		if (dip_num(w, h, mid) >= n)
+			r = mid;
+		else
+			l = mid;
+	}
+	return r;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	ull w, h, n;
+	cin >> w >> h >> n;
+
+	cout << find_ans(w, h, n) << "\n";
+
+	return 0;
+}
